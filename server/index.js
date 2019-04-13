87746e168dc5c1c1eaf14dca125f576444a632ad
Theo Manton <theo333@gmail.com>
2019-04-13 11:07:19 -0400

    set up students and schools api routes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const { Student, School, syncAndSeed } = require('./db');
+
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.get('/app.js', (req, res, next) =>
 	res.sendFile(path.join(__dirname, '../dist', 'main.js'))
@@ -11,5 +15,20 @@ app.get('/app.js', (req, res, next) =>
 app.get('/', (req, res, next) =>
 	res.sendFile(path.join(__dirname, '../client', 'index.html'))
 );
+
+// api routes
+app.get('/api/students', (req, res, next) => {
+	Student.findAll()
+		.then(students => res.json(students))
+		.catch(next);
+});
+
+app.get('/api/schools', (req, res, next) => {
+	School.findAll()
+		.then(schools => res.json(schools))
+		.catch(next);
+});
+
+syncAndSeed();
 
 app.listen(port, () => console.log(`listening on port ${port}`));

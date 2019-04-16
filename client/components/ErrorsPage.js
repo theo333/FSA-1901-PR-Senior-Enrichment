d495cc81f03bbb6db4746f111eae2a86cd6c9215
Theo Manton <theo333@gmail.com>
2019-04-16 14:49:00 -0400

    tweak to errors page

--- client/components/ErrorsPage.js
+++ client/components/ErrorsPage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const ErrorsPage = () => {
 	return (
 		<div>
-			<h2>Sorry, this page does not exist</h2>
+			<h2>Oops! This page does not exist. :)</h2>
 			<Link to='/schools'>Return to Home Page</Link>
 		</div>
 	);
